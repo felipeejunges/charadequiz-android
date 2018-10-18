0ecdebef1f0962fac565c97f92c2382e31de77f8
Felipe Junges <felipeejunges@hotmail.com>
2018-10-17 21:14:55 -0300

    Primeiro Commit

---
+++ app/src/main/java/br/com/desafios/charadequiz/Controller/LoginActivity.java
@@ -0,0 +1,15 @@
+package br.com.desafios.charadequiz.Controller;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import br.com.desafios.charadequiz.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
