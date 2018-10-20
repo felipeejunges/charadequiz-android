e4b378057b10672d285085e40dbf64e38408cc27
Felipe Junges <felipeejunges@hotmail.com>
2018-10-20 05:06:41 -0300

    Ajustes

--- app/src/main/java/br/com/desafios/charadequiz/Controller/MainActivity.java
+++ app/src/main/java/br/com/desafios/charadequiz/Controller/MainActivity.java
@@ -1,15 +1,19 @@
 package br.com.desafios.charadequiz.Controller;
 
-import android.app.Activity;
-import android.service.autofill.Dataset;
-import android.support.v7.app.AppCompatActivity;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
 
 import br.com.desafios.charadequiz.R;
 import br.com.desafios.charadequiz.Singleton.DataStore;
-import br.com.desafios.charadequiz.preferences.UsuarioLoginPreferences;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +25,41 @@ public class MainActivity extends Activity {
         DataStore.sharedInstance().getPreferences().naoEstaLogado();
 
         // Toda a logica
+    }
+
+    public void onClickIWantAChallange(View v) {
+        Intent intent = new Intent(MainActivity.this, QuestionActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK
+                | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NO_HISTORY);
+        startActivity(intent);
+        finish();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()) {
+            case R.id.menuLogout:
+                AlertDialog.Builder message = new AlertDialog.Builder(this);
+                message.setTitle("Tem certeza que deseja realizar logout?");
+                message.setPositiveButton("Sim", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        DataStore.sharedInstance().getPreferences().deslogar();
+                    }
+                });
+                message.setNegativeButton("Não", null);
+                message.show();
+                break;
+        }
+        return true;
     }
 
     @Override
